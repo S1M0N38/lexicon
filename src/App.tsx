f1497ab9b3689eb5cccb8972ffe2e880db838e99
S1M0N38 <bertolottosimone@gmail.com>
2025-03-17 21:54:07 +0100

    fix: move from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Change BrowserRouter to HashRouter
 import Index from "./pages/Index";
 import WordDetail from "./pages/WordDetail";
 import Browse from "./pages/Browse";
@@ -13,16 +13,15 @@ import { ThemeProvider } from "./components/ThemeProvider";
 
 const queryClient = new QueryClient();
 
-// Get the base URL from the environment or use '/' for development
-const baseUrl = import.meta.env.BASE_URL || "/";
-
 const App = () => (
   <ThemeProvider defaultTheme="system">
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename={baseUrl}>
+        <HashRouter>
+          {" "}
+          {/* Changed from BrowserRouter, removed basename */}
           <Navbar />
           <Routes>
             <Route path="/" element={<Index />} />
@@ -30,7 +29,7 @@ const App = () => (
             <Route path="/browse" element={<Browse />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </ThemeProvider>
